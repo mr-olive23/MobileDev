681660c2f270fe0fc20b8c21c7b52651a8f071d3
Rijst <klaus.daems@outlook.com>
2018-12-18 14:12:28 +0100

    Say it yourself - Ophalen van foto's

--- LogopedieProject/app/src/main/java/be/thomasmore/logopedieproject/SayItYourselfActivity.java
+++ LogopedieProject/app/src/main/java/be/thomasmore/logopedieproject/SayItYourselfActivity.java
@@ -1,17 +1,28 @@
 package be.thomasmore.logopedieproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
 public class SayItYourselfActivity extends AppCompatActivity {
+    ArrayList<String> pictures = new ArrayList<String>();
+    String pictureName;
 
     private int juistAntwoord;
 
@@ -23,12 +34,30 @@ public class SayItYourselfActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //get location
+        Intent intent = getIntent();
+        pictures = intent.getStringArrayListExtra("navPosition");
+
+        //get auditing bombardment file
+        String jsonText = loadJSONFromAsset(getApplicationContext());
+        try {
+            JSONObject config = new JSONObject(jsonText);
+            JSONObject temp = config;
+            for (int i = 0; i < pictures.size(); i++){
+                temp = temp.getJSONObject(pictures.get(i));
+            }
+            pictureName = temp.getString("picture");
+            Log.e("gogo", pictureName);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
         maakPrentjes();
     }
     public void maakPrentjes(){
         //Prentjes shufflen
         Intent intent = getIntent();
-        
+
         //Prentjes tonen
     }
 
@@ -43,6 +72,30 @@ public class SayItYourselfActivity extends AppCompatActivity {
     }
 
     public void onClickDraaiPrent(){
+
+    }
+
+    private String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("config.json");
+
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
 
     }
 }
