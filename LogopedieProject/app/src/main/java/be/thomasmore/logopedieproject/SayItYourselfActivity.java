d046a33e3118c8e8dcd54f0860dd29b765f8f1ce
Rijst <klaus.daems@outlook.com>
2019-01-03 19:51:28 +0100

    SayItYourself - Compleet

--- LogopedieProject/app/src/main/java/be/thomasmore/logopedieproject/SayItYourselfActivity.java
+++ LogopedieProject/app/src/main/java/be/thomasmore/logopedieproject/SayItYourselfActivity.java
@@ -3,7 +3,9 @@ package be.thomasmore.logopedieproject;
 import android.animation.Animator;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +17,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,8 +32,8 @@ public class SayItYourselfActivity extends AppCompatActivity {
     JSONObject word1, word2;
     String picture1, picture2;
 
-    private int juistAntwoord;
-
+    private int correctButtonID;
+    ImageView ivShowing = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,6 +55,16 @@ public class SayItYourselfActivity extends AppCompatActivity {
             }
             picture1 = temp.getJSONObject("word1").getString("picture");
             picture2 = temp.getJSONObject("word2").getString("picture");
+
+            String word1 = temp.getJSONObject("word1").getString("word");
+            String word2 = temp.getJSONObject("word2").getString("word");
+
+            Button b_1 = (Button) findViewById(R.id.answer_1);
+            Button b_2 = (Button) findViewById(R.id.answer_2);
+
+            b_1.setText(word1);
+            b_2.setText(word2);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -62,28 +75,74 @@ public class SayItYourselfActivity extends AppCompatActivity {
         boolean number = r.nextBoolean();
         if(number) {
             int id = getResources().getIdentifier(picture1, "drawable",getPackageName());
+            correctButtonID = R.id.answer_1;
             return ContextCompat.getDrawable(getApplicationContext(), id);
         }else{
             int id = getResources().getIdentifier(picture2, "drawable",getPackageName());
+            correctButtonID = R.id.answer_2;
             return ContextCompat.getDrawable(getApplicationContext(), id);
         }
     }
 
     public void onClickAnswer(View v) {
-        Button answer = (Button) v;
-        int buttonTag = Integer.parseInt(v.getTag().toString());
-        if (buttonTag == juistAntwoord) {
-            // prent blijft dan omgedraaid + pling geluid
+        int clickedButtonID = v.getId();
+        final MediaPlayer goed = MediaPlayer.create(this, R.raw.goedzo);
+        final MediaPlayer fout = MediaPlayer.create(this, R.raw.bijna_goed_nog_eens);
+        if (clickedButtonID == correctButtonID) {
+            // JUIST
+            Toast.makeText(this, "CORRECT", Toast.LENGTH_SHORT).show();
+            goed.start();
+
         } else {
-            // prent word terug omgedraaid
+            // FOUT
+            Toast.makeText(this, "FOUT", Toast.LENGTH_SHORT).show();
+            draaiOpnieuwOmNaarVraagteken();
+            fout.start();
+
         }
+
+    }
+
+    private void draaiOpnieuwOmNaarVraagteken(){
+        Resources resources = getApplicationContext().getResources();
+        final int vraagtekenResourceID = resources.getIdentifier("vraagteken", "drawable", getApplicationContext().getPackageName());
+        ivShowing.setRotation(0f);
+        ivShowing.animate().rotationY(90f).setListener(new Animator.AnimatorListener(){
+            @Override
+            public void onAnimationStart(Animator animation)
+            {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation)
+            {
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation)
+            {
+                ivShowing.setImageResource(vraagtekenResourceID);
+                ivShowing.setRotationY(270f);
+                ivShowing.animate().rotationY(360f).setListener(null);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation)
+            {
+            }
+        });
+    }
+
+    public void onClickTerug(View v) {
+
+
     }
 
     public void onClickDraaiPrent(View v) {
         final Drawable drawable=randomDrawable();
-        final ImageView iv = (ImageView) findViewById(v.getId());
-        iv.setRotationY(0f);
-        iv.animate().rotationY(90f).setListener(new Animator.AnimatorListener()
+        ivShowing = (ImageView) findViewById(v.getId());
+        ivShowing.setRotationY(0f);
+        ivShowing.animate().rotationY(90f).setListener(new Animator.AnimatorListener()
         {
 
             @Override
@@ -99,9 +158,9 @@ public class SayItYourselfActivity extends AppCompatActivity {
             @Override
             public void onAnimationEnd(Animator animation)
             {
-                iv.setImageDrawable(drawable);
-                iv.setRotationY(270f);
-                iv.animate().rotationY(360f).setListener(null);
+                ivShowing.setImageDrawable(drawable);
+                ivShowing.setRotationY(270f);
+                ivShowing.animate().rotationY(360f).setListener(null);
             }
 
             @Override
